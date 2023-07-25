18c4f653b1d8417e49a4216764c60451cdb731d5
marcabounader92 <marcabounader92@outlook.com>
2023-07-25 21:56:13 +0300

    JS: add token local storage with status 1

--- src/js/login.js
+++ src/js/login.js
@@ -20,11 +20,13 @@ document.getElementById("submit").addEventListener("click", (event) => {
         
     }).then(response => response.json())
       .then(response => {
-          if (response["status"]) {            
+          if (response["status"]=="1") {            
             console.log('status: ' + response.status)
             console.log('response: ' + response.id)
             localStorage.setItem("id", response.id)
             console.log('id: ' + localStorage.getItem("id"))
+            localStorage.setItem("token_value",response.token_value)
+            console.log(localStorage.getItem("token_value"));
             window.location.href = "./src/html/classes.html";
           } else {
               console.log(response["message"]);
