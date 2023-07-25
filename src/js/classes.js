21345471a487e348aabd443f28bedf24d1f1bc8b
MHoussam <mohammadosta1999@gmail.com>
2023-07-25 11:24:52 +0300

    Front-end <> Adding the local storage to the js pages.

--- src/js/classes.js
+++ src/js/classes.js
@@ -1,16 +1,32 @@
 document.addEventListener("DOMContentLoaded", getClasses);
 
 let classesArray = []
+const id = localStorage.getItem("id");
+const getClassesFromID = {
+    student_id : id
+};
 
 function getClasses() {
-  fetch("http://localhost/Google-Classroom-Clone_Backend/get-student-classes.php")
+  fetch("http://127.0.0.1/Google-Classroom-Clone_Backend/get-student-classes.php", {
+        method: "POST",
+        mode: 'cors',
+        cache: "no-cache",
+        origin: "http://127.0.0.1:5500",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getClassesFromID),
+    })
     .then((response) => response.json())
     .then((class_student) => {
       classesArray = class_student;
+      console.log(class_student)
       displayPosts()
     })
     .catch((error) => console.log(error))
 }
+console.log(getClassesFromID)
 
 function displayPosts() {
   const classesList = document.getElementById("classes");
@@ -22,7 +38,7 @@ function displayPosts() {
         <div class="class-up">
             <div class="class-title-options flex">
                 <div class="class-up-titles flex-column">
-                    <a href="./studentClass.html" class="link">
+                    <a href="./studentClass.html" class="link" onclick="getClassId(${class_student.class_id})" >
                         <div class="class-title">${class_student.class_name}</div>
 
                         <div class="class-sub-title">${class_student.section}</div>
@@ -55,7 +71,7 @@ function displayPosts() {
         
         <div class="class-end flex pointer">
             <div class="your-work">
-                <abbr title='Open your work for "FSW 23& 24 | Tech"'>
+                <abbr title='Open your work for "${class_student.class_name}"'>
                     <svg focusable="false" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7.55 0c.14-.15.33-.25.55-.25s.41.1.55.25c.12.13.2.31.2.5 0 .41-.34.75-.75.75s-.75-.34-.75-.75c0-.19.08-.37.2-.5zM19 5v10.79C16.52 14.37 13.23 14 12 14s-4.52.37-7 1.79V5h14zM5 19v-.77C6.74 16.66 10.32 16 12 16s5.26.66 7 2.23V19H5z"></path>
                         <path d="M12 13c1.94 0 3.5-1.56 3.5-3.5S13.94 6 12 6 8.5 7.56 8.5 9.5 10.06 13 12 13zm0-5c.83 0 1.5.67 1.5 1.5S12.83 11 12 11s-1.5-.67-1.5-1.5S11.17 8 12 8z"></path>
@@ -64,7 +80,7 @@ function displayPosts() {
             </div>
 
             <div class="folder">
-                <abbr title='Open folder for "FSW 23& 24 | Tech Full Stack Web Development Bootcamp" in google drive'>
+                <abbr title='Open folder for "${class_student.class_name} ${class_student.section}" in google drive'>
                     <svg focusable="false" width="24" height="24" viewBox="0 0 24 24">
                         <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"></path>
                     </svg>
@@ -73,7 +89,16 @@ function displayPosts() {
         </div>
     </div>
     `;
+
+    //console.log(class_student.class_id);
     classesList.appendChild(listItem)
   })
+}
 
+//.addEventListener('onclick', getClassId);
+
+function getClassId (class_id) {
+    //console.log("the captain: " + class_id);
+    localStorage.setItem("class_id", class_id);
+    console.log('class_id: ' + localStorage.getItem("class_id"));
 }
--- src/js/studentClass.js
+++ src/js/studentClass.js
@@ -1,7 +1,23 @@
 document.addEventListener("DOMContentLoaded", getMaterials);
 
+const class_id = localStorage.getItem("class_id");
+
+const getClassesFromID = {
+    class_id: class_id
+};
+
 function getMaterials() {
-  fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-materials.php")
+  fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-materials.php", {
+    method: "POST",
+    mode: 'cors',
+    cache: "no-cache",
+    origin: "http://127.0.0.1:5500",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(getClassesFromID),
+})
     .then((response) => response.json())
     .then((class_material) => {
       materialsArray = class_material;
@@ -16,7 +32,7 @@ function displayMaterials() {
   materialsArray.forEach((class_material) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-    <a href="./assignmentClass.html" class="link">
+    <a href="./materialClass.html" class="link" onclick="getMaterialId(${class_material.material_id})">
         <div class="post flex font-google-sans bold pointer">
             <div class="post-left flex">
                 <div class="material flex">
@@ -60,10 +76,20 @@ function displayMaterials() {
 }
 
 function getAssignments() {
-    fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-assignments.php")
+    fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-assignments.php", {
+        method: "POST",
+        mode: 'cors',
+        cache: "no-cache",
+        origin: "http://127.0.0.1:5500",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getClassesFromID),
+    })
       .then((response) => response.json())
-      .then((assignment) => {
-        assignmentsArray = assignment;
+      .then((class_assignment) => {
+        assignmentsArray = class_assignment;
         displayAssignments()
       })
       .catch((error) => console.log(error))
@@ -72,27 +98,28 @@ function getAssignments() {
 function displayAssignments() {
     const assignmentsList = document.getElementById("posts");
     assignmentsList.innerHTML += "";
-    assignmentsArray.forEach((assignment) => {
+    assignmentsArray.forEach((class_assignment) => {
       const listItem = document.createElement("li");
       listItem.innerHTML = `
-      <a href="./sign.html" class="link">
+      <a href="./assignmentClass.html" class="link" onclick="getAssignmentId(${class_assignment.assignment_id})">
         <div class="post flex font-google-sans bold pointer">
             <div class="post-left flex">
                 <div class="material flex">
                     <div class="circle flex">
                         <svg class="material-icon" focusable="false" width="24" height="24" viewBox="0 0 24 24">
-                            <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+                        <path d="M7 15h7v2H7zm0-4h10v2H7zm0-4h10v2H7z"></path>
+                        <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1c-1.3 0-2.4.84-2.82 2H5c-.14 0-.27.01-.4.04a2.008 2.008 0 0 0-1.44 1.19c-.1.23-.16.49-.16.77v14c0 .27.06.54.16.78s.25.45.43.64c.27.27.62.47 1.01.55.13.02.26.03.4.03h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7-.25c.41 0 .75.34.75.75s-.34.75-.75.75-.75-.34-.75-.75.34-.75.75-.75zM19 19H5V5h14v14z"></path>
                         </svg>
                     </div>
                 </div>
                 
                 <div class="post-description flex-column">
                     <div class="post-title">
-                        X posted a new assignment: ${assignment.title}
+                        X posted a new assignment: ${class_assignment.title}
                     </div>
                     
                     <div class="post-date">
-                        ${assignment.due_date}
+                        ${class_assignment.due_date}
                     </div>
                 </div>
             </div>
@@ -112,6 +139,17 @@ function displayAssignments() {
     </a>
     `;
     assignmentsList.appendChild(listItem)
-    })
-  
+    }) 
+}
+
+function getMaterialId (material_id) {
+    //console.log("the captain: " + class_id);
+    localStorage.setItem("material_id", material_id);
+    console.log('material_id: ' + localStorage.getItem("material_id"));
+}
+
+function getAssignmentId (assignment_id) {
+    //console.log("the captain: " + class_id);
+    localStorage.setItem("assignment_id", assignment_id);
+    console.log('assignment_id: ' + localStorage.getItem("assignment_id"));
 }
