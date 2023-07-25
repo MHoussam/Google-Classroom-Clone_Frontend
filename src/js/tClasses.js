a208154293c498ac755fc91fc79e36e787d0030c
MHoussam <mohammadosta1999@gmail.com>
2023-07-25 08:17:05 +0300

    Front-end <> Added the updated info page and the teacher's section for posting a new assignment.

---
+++ src/js/tClasses.js
@@ -0,0 +1,79 @@
+document.addEventListener("DOMContentLoaded", getClasses);
+
+let classesArray = []
+
+function getClasses() {
+  fetch("http://localhost/Google-Classroom-Clone_Backend/get-teacher-classes.php")
+    .then((response) => response.json())
+    .then((class_teacher) => {
+      classesArray = class_teacher;
+      displayPosts()
+    })
+    .catch((error) => console.log(error))
+}
+
+function displayPosts() {
+  const classesList = document.getElementById("classes");
+  classesList.innerHTML = "";
+  classesArray.forEach((class_teacher) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+    <div class="class flex-column pointer">
+        <div class="class-up">
+            <div class="class-title-options flex">
+                <div class="class-up-titles flex-column">
+                    <a href="./studentClass.html" class="link">
+                        <div class="class-title">${class_teacher.class_name}</div>
+
+                        <div class="class-sub-title">${class_teacher.section}</div>
+                    </a>
+                </div>
+
+                <div class="class-options-container flex">
+                    <div class="class-options flex-column">
+                        <div class="google-apps-circle"></div>
+
+                        <div class="google-apps-circle"></div>
+
+                        <div class="google-apps-circle"></div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="description"><a class="link">Tech Department</a></div>
+        </div>
+        
+        <img src="/src/assets/images/user-profile.png" class="user-profile">
+        
+        <div class="class-middle">
+            <div class="due">
+                <div class="due-date">Due Wednesday</div>
+
+                <div class="due-hour-first"><a href="" class="link-bk">10:00 AM – Google Classroomasd sdf sda s</a></div>
+            </div>
+        </div>
+        
+        <div class="class-end flex pointer">
+            <div class="your-work">
+                <abbr title='Open your work for "FSW 23& 24 | Tech"'>
+                    <svg focusable="false" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M19 3h-4.18C14.4 1.84 13.3 1 12 1s-2.4.84-2.82 2H5c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm-7.55 0c.14-.15.33-.25.55-.25s.41.1.55.25c.12.13.2.31.2.5 0 .41-.34.75-.75.75s-.75-.34-.75-.75c0-.19.08-.37.2-.5zM19 5v10.79C16.52 14.37 13.23 14 12 14s-4.52.37-7 1.79V5h14zM5 19v-.77C6.74 16.66 10.32 16 12 16s5.26.66 7 2.23V19H5z"></path>
+                        <path d="M12 13c1.94 0 3.5-1.56 3.5-3.5S13.94 6 12 6 8.5 7.56 8.5 9.5 10.06 13 12 13zm0-5c.83 0 1.5.67 1.5 1.5S12.83 11 12 11s-1.5-.67-1.5-1.5S11.17 8 12 8z"></path>
+                    </svg>
+                </abbr> 
+            </div>
+
+            <div class="folder">
+                <abbr title='Open folder for "FSW 23& 24 | Tech Full Stack Web Development Bootcamp" in google drive'>
+                    <svg focusable="false" width="24" height="24" viewBox="0 0 24 24">
+                        <path d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V8h16v10z"></path>
+                    </svg>
+                </abbr> 
+            </div>
+        </div>
+    </div>
+    `;
+    classesList.appendChild(listItem)
+  })
+
+}
---
+++ src/js/teacherClass.js
@@ -0,0 +1,164 @@
+document.addEventListener("DOMContentLoaded", getMaterials);
+
+function getMaterials() {
+  fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-materials.php")
+    .then((response) => response.json())
+    .then((class_material) => {
+      materialsArray = class_material;
+      displayMaterials()
+    })
+    .catch((error) => console.log(error))
+}
+
+function displayMaterials() {
+  const materialsList = document.getElementById("posts");
+  materialsList.innerHTML = "";
+  materialsArray.forEach((class_material) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+    <a href="./assignmentClass.html" class="link">
+        <div class="post flex font-google-sans bold pointer">
+            <div class="post-left flex">
+                <div class="material flex">
+                    <div class="circle flex">
+                        <svg class="material-icon" focusable="false" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+                        </svg>
+                    </div>
+                </div>
+                
+                <div class="post-description flex-column">
+                    <div class="post-title">
+                        X posted a new material: ${class_material.title}
+                    </div>
+                    
+                    <div class="post-date">
+                        ${class_material.date_of_upload}
+                    </div>
+                </div>
+            </div>
+
+            <div class="post-right flex">
+                <div class="post-copy-link flex">
+                    <div class="copy-link-circles flex-column">
+                        <div class="google-apps-circle"></div>
+                        
+                        <div class="google-apps-circle"></div>
+                
+                        <div class="google-apps-circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+    `;
+    materialsList.appendChild(listItem)
+  })
+
+  getAssignments();
+
+}
+
+function getAssignments() {
+    fetch("http://localhost/Google-Classroom-Clone_Backend/get-class-assignments.php")
+      .then((response) => response.json())
+      .then((assignment) => {
+        assignmentsArray = assignment;
+        displayAssignments()
+      })
+      .catch((error) => console.log(error))
+}
+  
+function displayAssignments() {
+    const assignmentsList = document.getElementById("posts");
+    assignmentsList.innerHTML += "";
+    assignmentsArray.forEach((assignment) => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
+      <a href="./sign.html" class="link">
+        <div class="post flex font-google-sans bold pointer">
+            <div class="post-left flex">
+                <div class="material flex">
+                    <div class="circle flex">
+                        <svg class="material-icon" focusable="false" width="24" height="24" viewBox="0 0 24 24">
+                            <path d="M18 2H6c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h12c1.1 0 2-.9 2-2V4c0-1.1-.9-2-2-2zm0 18H6V4h2v8l2.5-1.5L13 12V4h5v16z"></path>
+                        </svg>
+                    </div>
+                </div>
+                
+                <div class="post-description flex-column">
+                    <div class="post-title">
+                        X posted a new assignment: ${assignment.title}
+                    </div>
+                    
+                    <div class="post-date">
+                        ${assignment.due_date}
+                    </div>
+                </div>
+            </div>
+    
+            <div class="post-right flex">
+                <div class="post-copy-link flex">
+                    <div class="copy-link-circles flex-column">
+                        <div class="google-apps-circle"></div>
+                        
+                        <div class="google-apps-circle"></div>
+                
+                        <div class="google-apps-circle"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </a>
+    `;
+    assignmentsList.appendChild(listItem)
+    })
+  
+}
+
+document.getElementById('announce').addEventListener('onclick', openPost());
+
+function openPost() {
+
+}
+
+const textarea = document.getElementById('postText');
+
+textarea.addEventListener('focus', () => {
+  textarea.classList.add('focus');
+});
+
+let postText = document.getElementById('postText');
+let cancel = document.getElementById('cancel');
+let post = document.getElementById('post');
+
+cancel.addEventListener('click', function (){
+    postText.value = '';
+});
+
+post.addEventListener('click', function (){
+    fetch('http://localhost/Google-Classroom-Clone_Backend/add-post.php', {
+        method: "POST",
+        mode: 'cors',
+        cache: "no-cache",
+        origin: "http://localhost:5500",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postText.value), 
+    })
+    .then(response => response.json()) 
+    .then(response => {
+        if (response["status"]) {
+            // window.location.href = "index.html";
+            console.log(response);
+            //localStorage.setItem("email", registerCredentials.email);
+        } else {
+            console.log(response["message"]);
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+});
