2e7f193bc39697a6f48ad7dc1c219cf0908e3c29
adamhayek2002 <YOUR_EMAIL>
2023-07-26 09:42:50 +0300

    front-end <> adding add change password

---
+++ src/js/changePassword.js
@@ -0,0 +1,36 @@
+document.getElementById("create-account").addEventListener("click", (event) => {
+    event.preventDefault();
+      const newPassword = document.getElementById("new-password").value;
+      const reset_token = document.getElementById("reset-token").value;
+      
+      const getcredentials = {
+        email : localStorage.getItem("email"),
+        new_password: newPassword,
+        reset_token: reset_token,
+      };
+      fetch('http://localhost/Google-Classroom-Clone_Backend/student-forget-password-change.php', { 
+        method: "POST",
+        mode: 'cors',
+        cache: "no-cache",
+        origin: "http://localhost:5500",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(getcredentials),
+        
+    }).then(response => response.json())
+      .then(response => {
+          if (response["status"]) {
+              console.log(response)
+              if(response.status == 1){
+              }
+              
+          } else {
+              console.log(response["message"]);
+          }
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+  })
